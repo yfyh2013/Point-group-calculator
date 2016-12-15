1f0242ec4404bbca281ac796b5c70351decf1214
zmzeng <zm.zeng@foxmail.com>
2016-12-15 11:13:37 +0800

    Add files via upload

--- GP.m
+++ GP.m
@@ -2,7 +2,8 @@ clc;
 clear;
 %输入点群及可约表示，自动计算约化的系数
 %输入点群，返回相应的正交标表和简并度
-[Chart1,Dege1]=Return_point_group();
+disp('目前支持如下点群的计算(c3v oh td d2h d4h)')
+[Chart1,Dege1,ReRe1]=Return_point_group();
 %输入可约表示，并将其存为矩阵
 X0=input('请输入可约表示（数与数之间用空格隔开）','s');
 X=strread(X0,'%d');
@@ -18,5 +19,20 @@ while i<=length(X);
     Y(i)=1/h*Chart1(i,:)*Dege1*X;
     i=i+1;
 end
-disp(Y')
-
+i=1;flag=1;
+while i<=length(Y);
+	if rem(Y(i),1)~=0
+        fprintf('可约表示错误，无法化简');
+        flag=0;
+        break;
+    end
+    i=i+1;
+end
+i=1;
+while i<=length(Y)&&flag==1;
+    if Y(i)~=0
+        fprintf('%1.0f%s',Y(i),ReRe1{i});
+        fprintf(' + ');
+    end
+    i=i+1;
+end
